d8ec3913d16d8cf675370ca4aa7cd0c442713f8b
Frank Bishop <ffbishop3@gmail.com>
2021-02-12 12:09:44 -0800

    moved button

---
+++ main.js
@@ -0,0 +1,9 @@
+// var $modal = document.querySelector('.modal-container-closed');
+// var openButton = document.querySelector('.open-button');
+// var submitButton = document.querySelector('.submitButton');
+
+// openButton.addEventListener('click', openModal (event));
+
+// function openModal(event) {
+//   console.log('this works');
+// }
