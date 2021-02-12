3826859b02c2cc7185a5172432ac8373bbd11eb1
Osama Khan <osamaa.khan61@gmail.com>
2021-02-12 15:30:46 -0800

    Added some JS

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ var modalClassSelector = document.querySelector('.background-modal');
 var daySelect = document.querySelector('.day-select');
 var timeSelect = document.querySelector('.time-select');
 var textEntry = document.querySelector('.text-entry');
+var $daysOfTheWeek = document.querySelector('.row');
+var $seventhColumn = document.querySelectorAll('.one-seventh-column');
 openButton.addEventListener('click', openModal);
 
 submitButton.addEventListener('click', submitPlan);
-
 
 var data = {
   entries: [],
   editing: null,
   nextEntryId: 1
 };
-
 
 var inputs = {};
 
@@ -24,14 +24,24 @@ function openModal(event) {
 
 }
 
-function submitPlan (event) {
+function submitPlan(event) {
   event.preventDefault();
-
-
 
   modalClassSelector.className = 'background-modal hidden';
   console.log('it submits');
-  inputs.day = daySelect.value
-  inputs.time = timeSelect.value
-  inputs.description = textEntry.value
+  inputs.day = daySelect.value;
+  inputs.time = timeSelect.value;
+  inputs.description = textEntry.value;
+}
+
+$daysOfTheWeek.addEventListener('click', DayEntry);
+
+function DayEntry(event) {
+
+  for (var i = 0; i < $seventhColumn.length; i++) {
+    if (event.target === $seventhColumn[i]) {
+      var daySelector = $seventhColumn[i];
+    }
+  }
+  console.log(daySelector);
 }
