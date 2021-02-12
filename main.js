c8d0c1a25154510ac501070f7ae577ddbc837cd6
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-02-12 16:30:50 -0500

    done

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var $modal = document.querySelector('.modal-container-closed');
 var openButton = document.querySelector('.open-button');
 var submitButton = document.querySelector('.submit-button');
-
-openButton.addEventListener('click', openModal(event));
+var modalClassSelector = document.querySelector('.background-modal');
+openButton.addEventListener('click', openModal);
 
 function openModal(event) {
+  modalClassSelector.className = 'background-modal';
 
 }
